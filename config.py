e2565497df33cdd8c3f9f4fee63c97a59a3f1276
annafalpi27 <annafalpi@gmail.com>
2025-12-12 23:55:08 +0100

    feat: migrate db services to cloud

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 project_root = "./"
 
-chroma_configuration = {"collection_name": "news"}
+chroma_configuration = {
+    # "host": "localhost",
+    # "port": 8000,
+    "database": "the_news_hub",
+    "collection_name": "news"
+}
 
 mongo_configuration = {
     "host": "mongodb://localhost:27017/",
@@ -12,7 +17,6 @@ embeddings_configuration = {
     "endpoint": "https://models.github.ai/inference",
     "model": "openai/text-embedding-3-small",
 }
-
 
 news_api_configuration = {"url": "https://api.finlight.me/v2/", "endpoint": "articles/"}
 
